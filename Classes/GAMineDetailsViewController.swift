6ecb33a34d3bafbe44459681429a3544520d4ca1
侯佳男 <houjianan@houjianandeiMac.local>
2019-01-16 10:02:21 +0800

    0.0.1

---
+++ Classes/GAMineDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GAMineDetailsViewController.swift
+//  GAMineDetailsViewController
+//
+//  Created by 侯佳男 on 2019/1/15.
+//  Copyright © 2019年 侯佳男. All rights reserved.
+//
+
+import UIKit
+
+open class GAMineDetailsViewController: UIViewController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.orange
+        print("GAMineDetailsViewController")
+    }
+}
---
+++ GAMineDetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  GAMineDetailsViewController.h
+//  GAMineDetailsViewController
+//
+//  Created by 侯佳男 on 2019/1/15.
+//  Copyright © 2019年 侯佳男. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GAMineDetailsViewController.
+FOUNDATION_EXPORT double GAMineDetailsViewControllerVersionNumber;
+
+//! Project version string for GAMineDetailsViewController.
+FOUNDATION_EXPORT const unsigned char GAMineDetailsViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAMineDetailsViewController/PublicHeader.h>
+
+
